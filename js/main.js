6ddc68061a56e5c814d07da2af9fdc7ede0dcc53
alvinlin10 <alvinlin1010@gmail.com>
2017-03-29 21:11:50 -0700

    Final Lab

--- js/main.js
+++ js/main.js
@@ -25,6 +25,18 @@
 		// MODAL WINDOW POP-UP SLIDES ACROSS SCREEN TO CENTER-ALIGN
 		// COPY DISPLAYS "SOLD OUT - PLEASE SEE OTHER TOUR DATES FOR PURCHASE"
 	// WHEN USER CLOSES WINDOW, MODAL WINDOW FADES OUT SMOOTHLY
+
+$('.modal-background').hide();
+
+$('.open').on('click', function () {
+	$('.modal-background').css("display", "flex").hide().fadeIn();
+	$('.modal-content').fadeIn();
+});
+
+$('#close').on('click', function () {
+	$('.modal-background').fadeOut();
+	$('.modal-content').fadeOut();
+});
 //END SECTION 2: TOUR DATES
 
 
@@ -34,18 +46,75 @@
 //START SECTION 3: LIVE PERFORMANCES
 	// CAROUSEL SCRIPT
 	// https://www.w3schools.com/bootstrap/bootstrap_carousel.asp
-	// USER HAS THE OPTION TO VIEW 3 VIDEOS IN THE CAROUSEL 
-		// THERE IS A NEXT, PREVIOUS, AND nth INDICATION OF WHICH VIDEO YOU ARE VIEWING 
-		// IN RELATION TO THE OTHER VIDEOS
+	
+	// Set variable for imageNumber and initial value should be 1
+
+	// When the user clicks the next button
+		// If imageNumber is equal to 4
+			// Set imageNumber to 1
+	// Else
+		// Add one to imageNumber
+		// $('img:nth-child(' + imageNumber + ')')
+
+	// Use :nth-child to fade in current image
+
+
+// // Set variable for imageNumber and initial value should be 1
+//$("img").hide();
+// When the user clicks the next button
+	// Hide all images
+//var slideNumber = 1;
+//console.log(slideNumber);
+
+//$('img:nth-child(' + slideNumber + ')').show();
+
+
+//$("#next").click(function(){
+    //$('img:nth-child(' + slideNumber + ')').hide();
+
+
+	// If imageNumber is equal to 4
+//if (slideNumber === 4) {
+   //slideNumber = 1;
+//} else {
+	//slideNumber += 1
+//} 
+
+//$('img:nth-child(' + slideNumber + ')').fadeIn();
+
+//});
+
+// Previous
+
+//$("#previous").click(function(){
+    //$('img:nth-child(' + slideNumber + ')').hide();
+
+
+	// If imageNumber is equal to 4
+//if (slideNumber === 1) {
+   //slideNumber = 4;
+//} else {
+	//slideNumber -= 1
+//} 
+
+//$('img:nth-child(' + slideNumber + ')').fadeIn();
+
+//});
+
+		// Set imageNumber to 1
+
+
+	// Else
+
+		// Add one to imageNumber
+
+	// Use :nth-child to fade in current image
+
 //END SECTION 3: LIVE PERFORMANCE
 
 
 
 
-
-//START SECTION 4: ABOUT
-	// FLEXBOX
-//END SECTION SECTION 4: ABOUT
 
 
 
