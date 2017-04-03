98cc79f7d17a09230b47172af290d5f6837cf544
alvinlin10 <alvinlin1010@gmail.com>
2017-04-02 22:48:20 -0700

    Text + Image Update

--- js/main.js
+++ js/main.js
@@ -4,8 +4,48 @@
 	// https://www.w3schools.com/jquery/tryit.asp?filename=tryjquery_eff_animate_smoothscroll
 	// WHEN USER SELECTS NAVIGATION OPTION
 	// PAGE WILL SMOOTH SCROLL TO DESIRED SECTION WITH #SECTION-NAME
+	// When the user clicks on the hamburger-icon
+
+/* $('.hamburger-icon').on('click', function (e) {
+  e.preventDefault();
+
+  // Use jQuery to toggle the active class on the hamburger-icon that was clicked
+  $(this).toggleClass('active');
+});
+
+*/
 //END NAVIGATION SECTION
 
+
+
+// START SMOOTH SCROLLING
+
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("nav").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
+
+// END SMOOTH SCROLLING
 
 
 
@@ -17,7 +57,6 @@
 
 
 
-
 //START SECTION 2: TOUR DATES
 	// MODAL WINDOW - POP-UP "SOLD OUT"
 	// https://www.w3schools.com/bootstrap/bootstrap_modal.asp
@@ -25,7 +64,6 @@
 		// MODAL WINDOW POP-UP SLIDES ACROSS SCREEN TO CENTER-ALIGN
 		// COPY DISPLAYS "SOLD OUT - PLEASE SEE OTHER TOUR DATES FOR PURCHASE"
 	// WHEN USER CLOSES WINDOW, MODAL WINDOW FADES OUT SMOOTHLY
-
 $('.modal-background').hide();
 
 $('.open').on('click', function () {
@@ -41,80 +79,64 @@ $('#close').on('click', function () {
 
 
 
-
-
 //START SECTION 3: LIVE PERFORMANCES
 	// CAROUSEL SCRIPT
-	// https://www.w3schools.com/bootstrap/bootstrap_carousel.asp
-	
-	// Set variable for imageNumber and initial value should be 1
+// Set variable for imageNumber and initial value should be 1
+$("iframe").hide();
+// NEXT SLIDE
+var slideNumber = 1;
+console.log(slideNumber);
 
-	// When the user clicks the next button
-		// If imageNumber is equal to 4
-			// Set imageNumber to 1
-	// Else
-		// Add one to imageNumber
-		// $('img:nth-child(' + imageNumber + ')')
+$('iframe:nth-child(' + slideNumber + ')').show();
 
-	// Use :nth-child to fade in current image
-
-
-// // Set variable for imageNumber and initial value should be 1
-//$("img").hide();
-// When the user clicks the next button
-	// Hide all images
-//var slideNumber = 1;
-//console.log(slideNumber);
-
-//$('img:nth-child(' + slideNumber + ')').show();
-
-
-//$("#next").click(function(){
-    //$('img:nth-child(' + slideNumber + ')').hide();
-
+$("#next").click(function(){
+    $('iframe:nth-child(' + slideNumber + ')').hide();
 
 	// If imageNumber is equal to 4
-//if (slideNumber === 4) {
-   //slideNumber = 1;
-//} else {
-	//slideNumber += 1
-//} 
+if (slideNumber === 3) {
+   slideNumber = 1;
+} else {
+	slideNumber += 1
+} 
 
-//$('img:nth-child(' + slideNumber + ')').fadeIn();
+$('iframe:nth-child(' + slideNumber + ')').fadeIn();
+});
 
-//});
-
-// Previous
-
-//$("#previous").click(function(){
-    //$('img:nth-child(' + slideNumber + ')').hide();
-
+// PREVIOUS SLIDE
+$("#previous").click(function(){
+   $('iframe:nth-child(' + slideNumber + ')').hide();
 
 	// If imageNumber is equal to 4
-//if (slideNumber === 1) {
-   //slideNumber = 4;
-//} else {
-	//slideNumber -= 1
-//} 
+if (slideNumber === 1) {
+   slideNumber = 3;
+} else {
+	slideNumber -= 1
+} 
 
-//$('img:nth-child(' + slideNumber + ')').fadeIn();
-
-//});
-
-		// Set imageNumber to 1
-
-
-	// Else
-
-		// Add one to imageNumber
-
-	// Use :nth-child to fade in current image
-
+$('iframe:nth-child(' + slideNumber + ')').fadeIn();
+});
 //END SECTION 3: LIVE PERFORMANCE
 
 
+//START SECTION 4: CONNECT - SUBSCRIPTION
+	// MODAL WINDOW - POP-UP "SOLD OUT"
+	// https://www.w3schools.com/bootstrap/bootstrap_modal.asp
+	// WHEN USER SELECTS "BUY NOW" BUTTON OPTION
+		// MODAL WINDOW POP-UP SLIDES ACROSS SCREEN TO CENTER-ALIGN
+		// COPY DISPLAYS "WE'LL BE KEEPING IN TOUCH - PLEASE SEE OTHER TOUR DATES FOR PURCHASE"
+	// WHEN USER CLOSES WINDOW, MODAL WINDOW FADES OUT SMOOTHLY
+$('.modal-background-connect').hide();
 
+$('#submit-connect').on('click', function () {
+	$('.modal-background-connect').css("display", "flex").hide().fadeIn();
+	$('.modal-content-connect').fadeIn();
+});
 
+$('#connect-close').on('click', function () {
+	$('.modal-background-connect').fadeOut();
+	$('.modal-content-connect').fadeOut();
+});
+//END SECTION 4: CONNECT - SUBSCRIPTION
 
 
 
