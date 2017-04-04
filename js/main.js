b07bd9ce08822dcf062e42895dc34dbcfb2340b9
alvinlin10 <alvinlin1010@gmail.com>
2017-04-03 18:05:40 -0700

    FINAL PROJECT

--- js/main.js
+++ js/main.js
@@ -6,42 +6,29 @@
 	// PAGE WILL SMOOTH SCROLL TO DESIRED SECTION WITH #SECTION-NAME
 	// When the user clicks on the hamburger-icon
 
-/* $('.hamburger-icon').on('click', function (e) {
+ $('.hamburger-icon').on('click', function (e) {
   e.preventDefault();
 
   // Use jQuery to toggle the active class on the hamburger-icon that was clicked
   $(this).toggleClass('active');
+  $('#hide-nav').fadeToggle('medium');
+
+  // $('#hide-nav').removeClass('hide-now').fadeOut('fast');
+
 });
 
-*/
 //END NAVIGATION SECTION
 
 
 
 // START SMOOTH SCROLLING
-
-$(document).ready(function(){
-  // Add smooth scrolling to all links
-  $("nav").on('click', function(event) {
-
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
-
-      // Store hash
-      var hash = this.hash;
-
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, function(){
-   
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-      });
-    } // End if
+$(document).ready(function() {
+  $('a[href^="#"]').click(function() {
+      var target = $(this.hash);
+      if (target.length == 0) target = $('a[name="' + this.hash.substr(1) + '"]');
+      if (target.length == 0) target = $('html');
+      $('html, body').animate({ scrollTop: target.offset().top-100 }, 1000);
+      return false;
   });
 });
 
